bcc3940c1e46a134172c4722cf925dac0c21003f
João Alonso <joaoalonso42@gmail.com>
2021-02-23 12:56:23 -0300

    study c++

--- Estruturas de dados/C++/summary.cpp
+++ Estruturas de dados/C++/summary.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+using  std::cout;
+using std::cin;
+using std::endl;
 
-using namespace std;
+
+
+
 int main()
 {
-    /*int total;
+        
+    return 0;
+}
+
+/*
+    ---------------------
+
+//operações matématicos
+        int nun1, nun2;
+        
+        cout << "Digite o valor fr nun1: " << endl;
+        cin >> nun1;
+        cout << "Digite o valor fr nun2: " << endl;
+        cin >> nun2;
+
+        int som = nun1 + nun2;
+        int sub = nun1 - nun2;
+        float div = (float)nun1 / (float)nun2;
+        int mult = nun1 * nun2;
+        int res = nun1 % nun2;
+        
+        cout << "soma: " << som << endl;
+        cout << "subtração: " << sub << endl;
+        cout << "divisão: " << div << endl;
+        cout << "multiplicação: " << mult << endl;
+        cout << "resto: " << res << endl;
+
+    ----------------------------------------------
+// laço for
+    int total;
     for (int i = 0; i <= 20; i+=2){
         total += i;
     }
-    cout << "testando for: " << total <<endl;*/
+    cout << "testando for: " << total <<endl;
+    -------------------
+    
+// laço do while
     int num;
     int contador = 0;
     int soma = 0 ;
@@ -20,7 +57,8 @@ int main()
         contador++;
         cout <<"Contador: "<< contador <<endl; 
     } while (num != 0);
-    float media =(float)soma / (float)(contador) ;
+    float media =(float)soma / (float)(contador -1) ;
     cout << "A média da serie é " << media << endl;
-    return 0;
-}
+------------------------------------------------------
+
+    */
