05f8c5c2065959807364c5c40c92f20315e32682
João Alonso <joaoalonso42@gmail.com>
2021-02-23 19:43:31 -0300

    up

--- Estruturas de dados/C++/hello.cpp
+++ Estruturas de dados/C++/hello.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
-using namespace std;
-
+using std::cout;
+using std::cin;
+using std::endl;
+const int TOTAL = 20;
 int main()
 {
-    cout <<"Hello! World" << "\n";
+    int c[TOTAL];
+    for (int i = 0; i < TOTAL; i++){
+        c[i] = i*2;
+        //cout << "I: "<< i <<endl;
+        //cout << "C: " << c[i] <<endl;
+        
+    }
+    for (int i = 0; i < TOTAL; i++)
+    {
+        cout << " vetor c :" << c[i]<<endl;
+        
+    }
+    
+    
+
+
     return 0;
 }
