4b1ddac9d05ab933c815cd3a7e71ac764836be90
João Alonso <joaoalonso42@gmail.com>
2021-03-16 12:30:28 -0300

    class desfio2

---
+++ Estruturas de dados/douglas Maioli/Desafios02/desafio02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "desafio02.h"
+
+using std::cout;
+using std:: cin;
+using std:: cout;
+
+
+void Desafio02::ex06() const{
+    int familia[4];
+    cout << Enter 
+
+}
---
+++ Estruturas de dados/douglas Maioli/Desafios02/desafio02.h
@@ -0,0 +1,17 @@
+class Desafio02
+{
+private:
+    int x;
+    int y;
+public:
+    Desafio02();
+    ~Desafio02();
+
+    void ex06() const;//Calcula a média de famílias
+    void ex07() const;
+    void ex08() const;
+    void ex09() const;
+    void ex010() const;
+};
+
+
