611cc26b79cb5ce2f296cc4c53f48c8622d8a0cb
João Alonso <joaoalonso42@gmail.com>
2021-03-17 14:59:40 -0300

    classes

--- Estruturas de dados/douglas Maioli/Desafio03/cidades.cpp
+++ Estruturas de dados/douglas Maioli/Desafio03/cidades.cpp
@@ -5,8 +5,33 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-Cidades::Cidades(int ano = 0, int populacao =-1, char pais = 'Null'){
+Cidades::Cidades(char nome = 'cidade', int ano = 0, int populacao =-1, char pais = 'Null'){
     this->ano = ano;
     this->populacao = populacao;
-    pais = pais;
+    this->pais[50] = pais;
+    this->nome[50] = nome;
+}
+
+int Cidades::getAno(){
+    return ano;
+}
+
+int Cidades::getPopulacao(){
+    return populacao;
+}
+char Cidades::getPais(){
+    return 'pais';
+}
+void Cidades::setAno(int ano){
+    this->ano = ano;
+}
+void Cidades::setPais(char pais){
+    this->pais[50] = pais;
+}
+void Cidades::setPopulacao(int populacao){
+    this->populacao = populacao;
+}
+
+void Cidades::print(){
+    cout << "Cidade: "<< nome << "\n Pais:"<< pais <<"\nfundação: "<<ano<<"\npopulacao: " <<populacao << endl;
 }
--- Estruturas de dados/douglas Maioli/Desafio03/cidades.h
+++ Estruturas de dados/douglas Maioli/Desafio03/cidades.h
@@ -1,20 +1,21 @@
 class Cidades
 {
 private:
+    char nome[50];//nome da cidade
     int ano; //ano de origem.
     int populacao;
     char pais[50];
 public:
-    Cidades(int ano, int populacao, char pais);
+    Cidades(char nome, int ano, int populacao, char pais);
     ~Cidades();
 
     void setAno(int ano);
-    void setPopilacao(int populacao);
+    void setPopulacao(int populacao);
     void setPais(char pais);
 
     int getAno();
     int getPopulacao();
-    char pais();
+    char getPais();
 
-    int print();
+    void print();
 };
