9814920f62d8fb60685173bcb7a39886600a408f
João Alonso <joaoalonso42@gmail.com>
2021-03-18 19:02:19 -0300

    class hash

--- Estruturas de dados/C++/HashColisões/hash.cpp
+++ Estruturas de dados/C++/HashColisões/hash.cpp
@@ -1,3 +1,21 @@
 #include"aluno.h"
 
-class hash{}
+class Hash{
+    private:
+    Aluno* structure;
+    int max_item;
+    int length;
+    int getHash(Aluno aluno);
+
+    public:
+    Hash(int max_item = 100);
+    ~Hash();
+
+    bool isFull();
+    int getlength();
+    int retriveItem(Aluno& Aluno, bool& found);
+    void deleteItem(Aluno aluno);
+    void insertItem(Aluno aluno);
+    void print();
+
+};
