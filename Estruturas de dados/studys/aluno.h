8fa4e2e81ea88c48f1d07314d430fb746007307d
João Alonso <joaoalonso42@gmail.com>
2021-03-20 09:31:33 -0300

    Create aluno.h

---
+++ Estruturas de dados/studys/aluno.h
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using std::string;
+
+class Aluno
+{
+    private:
+    int ra;
+    string name;
+
+    public:
+    Aluno();
+    Aluno(int ra, string name);
+
+    int getRa();
+    string getName();
+    
+};
+
+    
