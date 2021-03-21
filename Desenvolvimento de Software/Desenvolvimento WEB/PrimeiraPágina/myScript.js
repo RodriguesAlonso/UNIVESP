6452f079f07e32b86a1b01301b1f2fca61d5b8ab
João Alonso <joaoalonso42@gmail.com>
2021-03-21 17:10:56 -0300

    first codes Js

---
+++ Desenvolvimento de Software/Desenvolvimento WEB/PrimeiraPágina/myScript.js
@@ -0,0 +1,14 @@
+function soma() {
+    var num1 = 2;
+    var num2 = 5;
+    var soma = 0;
+    soma = num1 + num2;
+    return soma;
+}
+function situationShow(mean){
+    if(mean <=7 ){
+        alert("Passed");
+    }else{
+        alert("Failed")
+    }
+}
