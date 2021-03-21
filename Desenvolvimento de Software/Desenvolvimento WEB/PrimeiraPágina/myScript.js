540140f184d49553d183013b0f5a6c1774c682bb
João Alonso <joaoalonso42@gmail.com>
2021-03-21 17:22:43 -0300

    up

--- Desenvolvimento de Software/Desenvolvimento WEB/PrimeiraPágina/myScript.js
+++ Desenvolvimento de Software/Desenvolvimento WEB/PrimeiraPágina/myScript.js
@@ -1,12 +1,17 @@
-function soma() {
+function soma(num1, num2) {
     var num1 = 2;
     var num2 = 5;
     var soma = 0;
     soma = num1 + num2;
     return soma;
 }
-function situationShow(mean){
-    if(mean <=7 ){
+function mean(){
+    return mean = soma(10, 5)/10
+
+}
+function situationShow(){
+    mean = 10
+    if(mean >=7 ){
         alert("Passed");
     }else{
         alert("Failed")
