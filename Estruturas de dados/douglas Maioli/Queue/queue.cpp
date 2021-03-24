9df3e05c066cb0378224b0280d381ed66de3f9b2
João Alonso <joaoalonso42@gmail.com>
2021-03-24 12:19:03 -0300

    Update queue.cpp

--- Estruturas de dados/douglas Maioli/Queue/queue.cpp
+++ Estruturas de dados/douglas Maioli/Queue/queue.cpp
@@ -11,7 +11,7 @@ Queue::Queue(){
     rear = NULL;
 };
 Queue::~Queue(){
-    while (front != NULL && rear != NULL)
+    while (front != NULL)
     {
         NodeType* tempPtr;
         tempPtr = front;
@@ -39,15 +39,48 @@ bool Queue::isEmpty(){
 };
     
 void Queue::enQueue(ItemType item){
-    if (isFull())
+    if (!isFull())
     {
+        NodeType* newNode;
+        newNode = new NodeType;
+        newNode->info = item;
+        newNode->next = NULL;
+        if (rear == NULL)
+        {
+            front = newNode;
+        }else{
+            rear->next = newNode;
+            rear = newNode;
+        }
         
+    }else{
+        throw "Queue is already full.";
     }
     
 
 };
-ItemType Queue::deQueue(){
-
+ItemType Queue::deQueue()
+{
+    if (!isEmpty())
+    {
+        NodeType* tempNode;
+        tempNode = front;
+        ItemType item = front->info;
+        front = front->next;
+        if (front == NULL)
+        {
+            rear = NULL;
+            delete tempNode;
+        }
+    }else{
+           throw "Queue is empty.";
+    }
 };
-
-void print();
+void Queue::print(){
+    while (front != NULL)
+    {
+        cout << front->info << endl;
+        front = front->next;
+    }
+    
+}
