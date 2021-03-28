2274bbf551602c3a791e847f0e54dc96afbee3d1
João Alonso <joaoalonso42@gmail.com>
2021-03-28 15:52:56 -0300

    Update search_tree.h

---
+++ Estruturas de dados/Arvores/search_tree.h
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstddef>
+#include "aluno.h"
+
+struct Nodetype
+{
+    Aluno aluno;
+    Nodetype* esquerda;
+    Nodetype* direita;
+};
+
+class SearchTree{
+    public:
+    SearchTree();
+    ~SearchTree();
+
+    bool isFull() const;
+    bool isEmpty() const;
+    void retriveAluno(Aluno& item, bool& found) const;
+    
+    void insertAluno(Aluno item);
+    void deleteAluno(int intem);
+
+    void printerPreOrder() const;
+    void PrinterInOrder() const;
+    void PrinterPosOrder() const;
+};
