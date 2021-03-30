efd7fcca09007c68cf2111f1ec2bc8a07b01f6f1
João Alonso <joaoalonso42@gmail.com>
2021-03-30 12:22:30 -0300

    Update search_tree.h

--- Estruturas de dados/Arvores/search_tree.h
+++ Estruturas de dados/Arvores/search_tree.h
@@ -26,6 +26,17 @@ class SearchTree{
     void PrinterPosOrder() const;
 
     private:
+    Nodetype* root;
 
+    void destroyTree(Nodetype*& tree);
+    void retriveAluno(Nodetype* tree, Aluno& item, bool& found) const;
+    void insertAluno(Nodetype*& tree, Aluno item);
+    void deleteAluno(Nodetype*& tree, int aluno);
+    void deleteNode(Nodetype*& tree);
+    void getSucessor(Nodetype* tree, Aluno& data);
+    
+    void printerPreOrder(Nodetype* tree) const;
+    void printerInOrder(Nodetype* tree) const;
+    void printerPosOrder(Nodetype* tree) const;
     
 };
