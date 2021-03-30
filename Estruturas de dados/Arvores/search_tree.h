b24b407b7402ceb6a2daa315123d08a94441ab62
João Alonso <joaoalonso42@gmail.com>
2021-03-30 12:41:39 -0300

    Update search_tree.h

--- Estruturas de dados/Arvores/search_tree.h
+++ Estruturas de dados/Arvores/search_tree.h
@@ -11,32 +11,30 @@ struct Nodetype
 
 class SearchTree{
     public:
-    SearchTree();
-    ~SearchTree();
+    SearchTree() {root;}
+    ~SearchTree(){destroyTree(root);}
 
     bool isFull() const;
     bool isEmpty() const;
-    void retriveAluno(Aluno& item, bool& found) const;
+    void retriveAluno(Aluno& item, bool& found) const{retriveAluno(root, item, found);}
     
-    void insertAluno(Aluno item);
-    void deleteAluno(int intem);
+    void insertAluno(Aluno item) {insertAluno(root, item);}
+    void deleteAluno(int item) {deleteAluno(root, item);}
 
-    void printerPreOrder() const;
-    void PrinterInOrder() const;
-    void PrinterPosOrder() const;
+    void printerPreOrder() const {printerPreOrder(root);}
+    void PrinterInOrder() const {printerInOrder(root);}
+    void PrinterPosOrder() const {printerPosOrder(root);}
 
     private:
-    Nodetype* root;
-
-    void destroyTree(Nodetype*& tree);
-    void retriveAluno(Nodetype* tree, Aluno& item, bool& found) const;
-    void insertAluno(Nodetype*& tree, Aluno item);
-    void deleteAluno(Nodetype*& tree, int aluno);
-    void deleteNode(Nodetype*& tree);
-    void getSucessor(Nodetype* tree, Aluno& data);
-    
-    void printerPreOrder(Nodetype* tree) const;
-    void printerInOrder(Nodetype* tree) const;
-    void printerPosOrder(Nodetype* tree) const;
-    
+        Nodetype* root;
+        void destroyTree(Nodetype*& tree);
+        void retriveAluno(Nodetype* tree, Aluno& item, bool& found) const;
+        void insertAluno(Nodetype*& tree, Aluno item);
+        void deleteAluno(Nodetype*& tree, int aluno);
+        void deleteNode(Nodetype*& tree);
+        void getSucessor(Nodetype* tree, Aluno& data);
+        
+        void printerPreOrder(Nodetype* tree) const;
+        void printerInOrder(Nodetype* tree) const;
+        void printerPosOrder(Nodetype* tree) const;
 };
