6e8f8e2f2355a93c6851c96bca175b6ce04bcaef
João Alonso <joaoalonso42@gmail.com>
2021-04-01 20:55:06 -0300

    Update Queue.h

--- Estruturas de dados/Grafos/Queue.h
+++ Estruturas de dados/Grafos/Queue.h
@@ -1,6 +1,25 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "node_type.h"
 
+class Queue
+{
+    public:
+    Queue();
+    ~Queue();
+
+    bool isEmpyt() const;
+    bool isFull() const;
+    void print() const;
+
+    void enqueue(ItemType);
+    ItemType dequeue();
+
+    private:
+    NodeType* front;
+    NodeType* rear;
+
+};
 
 #endif
