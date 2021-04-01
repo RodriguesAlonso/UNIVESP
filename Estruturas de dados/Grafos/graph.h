bd2b300454c5c68166174e8c5f8262571550f7c1
João Alonso <joaoalonso42@gmail.com>
2021-04-01 20:58:43 -0300

    Create graph.h

---
+++ Estruturas de dados/Grafos/graph.h
@@ -0,0 +1,19 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "queue.h"
+
+class Graph
+{
+    private:
+    int NULL_EDGE;
+    int maxVertices;
+    int numVertices;
+    Vertex* vertices;
+    int** edges;
+    bool* mark;
+
+    
+};
+
+#endif
