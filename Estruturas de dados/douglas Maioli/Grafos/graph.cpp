03646e52563656ab912f69aa267319b346b25b16
João Alonso <joaoalonso42@gmail.com>
2021-04-07 18:18:02 -0300

    up

---
+++ Estruturas de dados/douglas Maioli/Grafos/graph.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "graph.h"
+
+using namespace std;
+
+    Graph::Graph(int maxVertices, int nullEdges){
+
+    }
+    Graph::~Graph(){
+
+    }
+    int Graph::getIndex(TypeItem item){
+
+    }
+    bool Graph::isFull(){
+
+    }
+    void Graph::insertVertex(TypeItem item){
+
+    }
+    void Graph::insertEdge(TypeItem end, TypeItem start, int weight){
+
+    }
+    int Graph::getWeight(TypeItem end, TypeItem start){
+
+    }
+    int Graph::getGrade(TypeItem item){
+
+    }
+    void Graph::printMatrix(){
+
+    }
+    void Graph::printVertex(){
+        
+    }
