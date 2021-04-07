951dab32659337ec0b93ae231cecc25f53d76711
João Alonso <joaoalonso42@gmail.com>
2021-04-07 17:45:21 -0300

    Update grafo.h

--- Estruturas de dados/douglas Maioli/Grafos/grafo.h
+++ Estruturas de dados/douglas Maioli/Grafos/grafo.h
@@ -1,2 +1,17 @@
 // Simple graph not directed weighted
 
+#include <iostream>
+
+using namespace std;
+
+typedef string TypeItem;
+
+class Grafo
+{ 
+    private:
+    int nullEdges;
+    int maxVertices;
+    int numVertices;
+    TypeItem vertex;
+    int** adjacentMatrix;
+};
