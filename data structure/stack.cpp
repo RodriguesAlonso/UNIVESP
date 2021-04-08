7cd82416a47bd3574d0bb7ce9ea287ef18056202
João Alonso <joaoalonso42@gmail.com>
2021-04-08 18:21:18 -0300

    Create stack.cpp

---
+++ data structure/stack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstddef>
+#include "stack.h"
+
+Stack::Stack(){
+    structure = NULL;
+}
+Stack::~Stack(){
+    NodeType* 
+
+}
+
+bool Stack::isFull() const{
+
+}
+bool Stack::isEmpty() const{
+    return (structure == NULL)
+
+}
+void Stack::printStack() const{
+
+}
+    
+void Stack::push(ItemType item){
+
+}
+ItemType Stack::pop(){
+
+}
