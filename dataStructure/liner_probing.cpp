f33047cfba1fb32d748c5624a5cbcd924e4666ba
João Alonso <joaoalonso42@gmail.com>
2021-04-12 15:47:46 -0300

    Update liner_probing.cpp

--- dataStructure/liner_probing.cpp
+++ dataStructure/liner_probing.cpp
@@ -11,9 +11,29 @@ Hash::~Hash(){
     delete [] structure;
 }
 
-bool Hash::isFull(){
+bool Hash::isFull() const{
     return (length == maxItem);
 }
-int Hash::getLength(){
-    return 
+int Hash::getLength() const{
+    return length;
+}
+int Hash::getHash(Student student){
+    return student.getRa() % maxItem;
+}
+
+void Hash::printHash() const{
+    for (int i = 0; i < maxItem; i++)
+    {
+        cout << i << structure[i].getName() << " => "<< structure[i].getRa() <<endl;
+    }
+}
+
+void Hash::insertItem(Student student){
+    int location = getHash(student);
+    while (structure[location].getRa() > 0)
+    {
+        location = (location + 1) % maxItem;
+    }
+    structure[location] = student;
+    length++;
 }
