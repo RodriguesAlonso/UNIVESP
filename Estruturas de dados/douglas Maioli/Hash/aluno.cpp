e61268e85379fcb99e553973755944464e4d081e
João Alonso <joaoalonso42@gmail.com>
2021-04-15 06:58:08 -0300

    Create aluno.cpp

---
+++ Estruturas de dados/douglas Maioli/Hash/aluno.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "Aluno.h"
+
+Aluno::Aluno(){
+    ra = -1;
+    nome = "";
+}
+Aluno::Aluno(string nome, int ra){
+    this->nome = nome;
+    this->ra = ra;
+}
+int Aluno::getRa(){
+    return ra;
+}
+string Aluno::getNome(){
+    return nome;
+}
