951dc1783926c93147fb6507984d6f67421f2c83
João Alonso <joaoalonso42@gmail.com>
2021-04-15 12:38:16 -0300

    Update mainHash.cpp

--- Estruturas de dados/douglas Maioli/Hash/mainHash.cpp
+++ Estruturas de dados/douglas Maioli/Hash/mainHash.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include "hash.h"
 
+
 using namespace std;
 
 int main()
 {
+    Hash alunoHash(5);
     Aluno aluno1("joao", 200);
     Aluno aluno2("Paulo", 201);
-    Hash alunoHash(3);
+    cout << "-=-=-=antes=-=-=-";
+    alunoHash.print();
     
     alunoHash.insertHash(aluno1);
     alunoHash.insertHash(aluno2);
+    cout << endl <<"-=-=-=depois-=-=-"<< endl;
+    alunoHash.print();
+    Aluno aluno3("", 201);
+    bool found = false;
+    alunoHash.retriveHash(aluno3, found);
+    cout << endl << "RA: "<< aluno3.getRa() << " Aluno: " << aluno3.getNome() << " -> " << found;
+    alunoHash.deleteAluno(aluno1);
+    cout << endl <<"-=-=-=FIM-=-=-";
     alunoHash.print();
     
     return 0;
