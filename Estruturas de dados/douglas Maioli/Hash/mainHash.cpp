35422a3702193a77f2f4edfac992b9a760dc2225
João Alonso <joaoalonso42@gmail.com>
2021-04-15 18:35:28 -0300

    Update mainHash.cpp

--- Estruturas de dados/douglas Maioli/Hash/mainHash.cpp
+++ Estruturas de dados/douglas Maioli/Hash/mainHash.cpp
@@ -8,15 +8,22 @@ int main()
 {
     Hash alunoHash(5);
     Aluno aluno1("joao", 200);
-    Aluno aluno2("Paulo", 201);
+    Aluno aluno2("Paulo", 205);
+    Aluno aluno4("Rodrigues", 210);
+    
     cout << "-=-=-=antes=-=-=-";
     alunoHash.print();
     
     alunoHash.insertHash(aluno1);
     alunoHash.insertHash(aluno2);
+    alunoHash.insertHash(aluno4);
+    
+    Aluno aluno5("Vicente", 215);
+    alunoHash.insertHash(aluno5);
+
     cout << endl <<"-=-=-=depois-=-=-"<< endl;
     alunoHash.print();
-    Aluno aluno3("", 201);
+    Aluno aluno3("", 210);
     bool found = false;
     alunoHash.retriveHash(aluno3, found);
     cout << endl << "RA: "<< aluno3.getRa() << " Aluno: " << aluno3.getNome() << " -> " << found;
