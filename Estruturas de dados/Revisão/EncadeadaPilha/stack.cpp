580a2c208b38d07f97f6f71a743505d2ca5591e1
João Alonso <joaoalonso42@gmail.com>
2021-04-17 17:00:34 -0300

    Create stack.cpp

---
+++ Estruturas de dados/Revisão/EncadeadaPilha/stack.cpp
@@ -0,0 +1,69 @@
+#include "stack.h"
+#include <iostream>
+#include <cstddef>
+#include <new>
+
+using namespace std;
+
+Stack::Stack(){
+    structure = NULL;
+}
+Stack::~Stack()
+{
+    nodeType* tempPtr;
+    while (structure != NULL)
+    {
+        tempPtr = structure;
+        structure = structure->next;
+        delete [] tempPtr;
+    }    
+}
+bool Stack::isFull(){
+    try
+    {
+        nodeType* newStructure;
+        newStructure = new nodeType;
+        delete newStructure;
+        return false;
+    }
+    catch(bad_alloc exception)
+    {
+        return true;
+    }
+    
+        
+}
+    bool Stack::isEmpty(){
+        return structure == NULL;        
+    }
+void Stack::push(itemType item){
+    if (!isFull())
+    {
+        nodeType* location = new nodeType;
+        location->info = item;
+        location->next = structure;
+        structure = location;
+    }else
+        throw "Stack is full";
+    }
+itemType Stack::pop(){
+    if (!isEmpty())
+    {
+        nodeType* tempPtr = structure;
+        itemType item = structure->info;
+        structure = structure->next;
+        delete tempPtr;
+        return item;
+    }else
+        throw "Stack is empty.";
+    }
+void Stack::print(){
+     cout << "Stack: ";
+     int i = 0;
+     while (structure != NULL)
+     {
+         cout << i << ": " << structure->info<<endl;
+         structure = structure->next;
+         i++;
+     }     
+    }
