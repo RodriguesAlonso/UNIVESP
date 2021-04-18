78fef4450018409a755573ce4f3b3bda49ccc239
João Alonso <joaoalonso42@gmail.com>
2021-04-18 10:29:58 -0300

    Create aluno.h

---
+++ Estruturas de dados/Revisão/EncadeadeFila/aluno.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::string;
+
+class Aluno{
+    public:
+    Aluno();
+    ~Aluno();
+    int getRa();
+    string getNome();
+    private:
+    int ra;
+    string nome;\
+
+
+};
