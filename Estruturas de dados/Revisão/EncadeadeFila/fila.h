28f8c2c47bf3d2bea61688fd65d1c600cd0c42b6
João Alonso <joaoalonso42@gmail.com>
2021-04-18 10:19:38 -0300

    Create fila.h

---
+++ Estruturas de dados/Revisão/EncadeadeFila/fila.h
@@ -0,0 +1,28 @@
+typedef int itemType;
+struct nodeType{
+
+    itemType info;
+    itemType front;
+    itemType rear;
+    nodeType* structure;
+
+};
+
+
+class Queue{
+    public:
+    Queue();
+    ~Queue();
+    bool isfull();
+    bool isEmpty();
+    void enqueue();
+    itemType deque();
+    void print();
+    private:
+    
+
+
+
+
+
+};
