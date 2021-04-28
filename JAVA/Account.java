48cdf1b4b2497e58571da1b5f37983e134a0e0d4
João Alonso <joaoalonso42@gmail.com>
2021-04-28 17:23:33 -0300

    Update Account.java

--- JAVA/Account.java
+++ JAVA/Account.java
@@ -1,6 +1,10 @@
 public class Account 
 {
     private String name; // Váriavel de instância.
+
+    public Account(String name){
+        this.name = name;
+    }
     
     public void setName(String name) 
     {
