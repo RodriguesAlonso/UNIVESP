618b572378b4e453f1d95f04ccddd460332c92cd
João Alonso <joaoalonso42@gmail.com>
2021-04-28 19:31:48 -0300

    Create testeConverter.java

---
+++ JAVA/EscalaTermometricas/testeConverter.java
@@ -0,0 +1,10 @@
+public class testeConverter {
+    public static void main(String[] args) {
+        Converter temperatura = new Converter();
+        temperatura.paraCelsius();
+        System.out.println("");
+        temperatura.paraFahrenheit();
+
+    }
+    
+}
