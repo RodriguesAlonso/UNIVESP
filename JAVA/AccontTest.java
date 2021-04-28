37a2f95a732db3407e9b967c37ec2ba63960e76e
João Alonso <joaoalonso42@gmail.com>
2021-04-28 18:48:30 -0300

    Update AccontTest.java

--- JAVA/AccontTest.java
+++ JAVA/AccontTest.java
@@ -1,37 +1,40 @@
 import java.util.Scanner;
 
-import jdk.vm.ci.code.stack.StackIntrospection;
-
 public class AccontTest 
 {
     public static void main(String[] args) 
     {
         Account account1 = new Account("Jane Green", 50.00);
         Account account2 = new Account("John Blue", -7.5);
-
-        System.out.printf("%n balance: $%.2f %n", account1.getBalance());
-        System.out.printf("%n balance: $%.2f %n", account2.getBalance());
-        //System.out.printf("account1 name is: %s%n", account1.getName());
-        //System.out.printf("account2 name is: %s%n", account2.getName());
+        
+        System.out.printf("%n%s balance: $%.2f", account1.getName(), account1.getBalance());
+        System.out.printf("%n%s balance: $%.2f %n", account2.getName(), account2.getBalance());
+        
         Scanner input = new Scanner(System.in);
-        //Account myAccount = new Account();
+
         System.out.print("Enter deposit amount for account1: ");
-        Double depositAmount = input.nextDouble();
+        double depositAmount = input.nextDouble();
         System.out.printf("%nAdding $%.2f to account1 balance%n%n", depositAmount);
+
         account1.deposit(depositAmount);
-
-        System.out.printf("%s balance: $%2.f %n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%2.f %n", account2.getName(), account2.getBalance());
-
+        
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n", account2.getName(), account2.getBalance());
+        
         System.out.print("Enter deposit amount for account2: ");
         depositAmount = input.nextDouble();
         System.out.printf("%nAdding $%.2f to account2 %n%n",depositAmount);
 
-        System.out.printf("%s balance: $%2.f %n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%2.f %n", account2.getName(), account2.getBalance());
+        account2.deposit(depositAmount);
         
+        System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n", account2.getName(), account2.getBalance());
+        
+        //System.out.printf("account1 name is: %s%n", account1.getName());
+        //System.out.printf("account2 name is: %s%n", account2.getName());
+        //Account myAccount = new Account();
         //System.out.printf("Initial name is: %s%n%n", //myAccount.getName());
-
+        
         //System.out.println("Please enter the name:");
         //String theName = input.nextLine();
         //myAccount.setName(theName);
