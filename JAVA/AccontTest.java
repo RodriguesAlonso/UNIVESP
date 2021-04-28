710d4f5004f1c046da1a885bcd874c45ef72fe89
João Alonso <joaoalonso42@gmail.com>
2021-04-28 17:38:32 -0300

    Update AccontTest.java

--- JAVA/AccontTest.java
+++ JAVA/AccontTest.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class AccontTest 
 {
-
     public static void main(String[] args) 
     {
-        Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
 
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
+        //Scanner input = new Scanner(System.in);
+        //Account myAccount = new Account();
 
-        System.out.println("Please enter the name:");
-        String theName = input.nextLine();
-        myAccount.setName(theName);
-        System.out.println();
+        //System.out.printf("Initial name is: %s%n%n", //myAccount.getName());
 
-        System.out.printf("Name in object myAccount is: %n%s%n", myAccount.getName());
+        //System.out.println("Please enter the name:");
+        //String theName = input.nextLine();
+        //myAccount.setName(theName);
+        //System.out.println();
+
+        //System.out.printf("Name in object myAccount is: %n%s%n", myAccount.getName());
     }
 }
