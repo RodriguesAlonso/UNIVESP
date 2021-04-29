81517521dc062b0caadc5c257b4e466978f71061
João Alonso <joaoalonso42@gmail.com>
2021-04-29 18:00:21 -0300

    Update testeConverter.java

--- JAVA/EscalaTermometricas/testeConverter.java
+++ JAVA/EscalaTermometricas/testeConverter.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
+
 public class testeConverter {
     public static void main(String[] args) {
-        Converter temperatura = new Converter();
-        temperatura.paraCelsius();
-        System.out.println("");
-        temperatura.paraFahrenheit();
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
 
+        System.out.println("Digite a temperatura em Fahrenheit ");
+        fahrenheit = input.nextDouble();        
+
+        System.out.printf("%n %.2f Fahrenheit = %.2f Celsius  %n", fahrenheit, Converter.paraCelsius(fahrenheit));
+        
+        System.out.println("Digite a temperatura em Celsius");
+        celsius = input.nextDouble();
+        System.out.printf("%n Celsius %2.f = %2.f fahrenheits %n", celsius, Converter.paraFahrenheit(celsius));
     }
     
 }
