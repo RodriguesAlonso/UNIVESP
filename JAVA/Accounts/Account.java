57c8f612bb36c271159ad4e0dd901173823b2880
João Alonso <joaoalonso42@gmail.com>
2021-04-30 10:32:34 -0300

    Update Account.java

--- JAVA/Accounts/Account.java
+++ JAVA/Accounts/Account.java
@@ -6,6 +6,11 @@ public class Account
     private String name; 
     private double balance;
 
+    public Account(){
+        name = "";
+        balance = 0.0;
+    }
+
     public Account(String name, double balance){
         this.name = name;
         if (balance > 0.0)
@@ -31,6 +36,15 @@ public class Account
     {
         return name;
     }
+    static Account createAccount(Account account){
+        String name = JOptionPane.showInputDialog("What is your name? ");
+        String balance = JOptionPane.showInputDialog(String.format("Welcome %s %nEnter your balance: " , name));
+        account.setName(name);
+        account.deposit(Double.parseDouble(balance));
+        
+        return account;        
+    }
+
     public void printAccount(){
         String menssage = String.format("%s balance: $%.2f %n", this.name, this.balance);
         JOptionPane.showMessageDialog(null, menssage);
