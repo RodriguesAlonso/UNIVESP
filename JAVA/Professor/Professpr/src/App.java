0e2024a5b45d99f86c299ccf926340dd1cd07e45
João Alonso <61242630+RodriguesAlonso@users.noreply.github.com>
2021-05-13 10:13:32 -0300

    Update App.java

--- JAVA/Professor/Professpr/src/App.java
+++ JAVA/Professor/Professpr/src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  
+    {
+        System.out.println("\nHello, World!");
+        ProfessorConc concursado;
+        concursado = new ProfessorConc();
+        concursado.setName("joao");
+        concursado.setSalario(3000);
+        System.out.printf("\nNome: %s Sálario:  %.2f\n", concursado.getName(), concursado.getSalario());
+
+        ProfessorHorista horista = new ProfessorHorista();
+        horista.setHoraTrabalho(20);
+        horista.setName("Paulo");
+        horista.calcSalario();
+        System.out.printf("Nome: %s %.2f \n", horista.getName(), horista.getSalario());
+
+
     }
 }
