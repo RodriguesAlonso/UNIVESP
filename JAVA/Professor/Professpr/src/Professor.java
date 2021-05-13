24558436e999be82c99e197aa0f3986528528b32
João Alonso <61242630+RodriguesAlonso@users.noreply.github.com>
2021-05-13 08:46:52 -0300

    Create Professor.java

---
+++ JAVA/Professor/Professpr/src/Professor.java
@@ -0,0 +1,27 @@
+public abstract class Professor {
+    private String name;
+    private double salario;
+
+    public Professor() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void calcSalario(){
+        
+    }
+}
