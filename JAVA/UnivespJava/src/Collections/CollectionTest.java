fff9c5e77f05b031edef4aef778a0580e6a3a006
João Alonso <joaoalonso42@gmail.com>
2021-06-17 15:21:24 -0300

    Update CollectionTest.java

--- JAVA/UnivespJava/src/Collections/CollectionTest.java
+++ JAVA/UnivespJava/src/Collections/CollectionTest.java
@@ -1,39 +1,18 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class CollectionTest {
-    
     public static void main(String[] args) {
-    // Adiciona elementos no array color a lista (Magenta, Red, White, Blue, Cyan).
-    String[] color = {"MAGENTA", "RED", "WHITE", "BLUE", "CYAN"};
-    ArrayList<String> colorList = new ArrayList<>();
-    for (String element : color)
-        colorList.add(element);
-    
-    //Adiciona elementos no array removeColor(Red, White, Blue) em remoList.
-    String[] removeColor = {"RED", "WHITE", "BLUE"};
-    ArrayList<String> removeColorList = new ArrayList<>();
-    for (String remove : removeColor)
-        removeColorList.add(remove);
-    //Print a lista.
-    System.out.printf("ColorList: %s%n", colorList);
-    System.out.printf("RemoveColorList: %s%n", removeColorList);
-    //Remove da lista as cores contidas em remoList.
-    removeColor(colorList, removeColorList);
-    //Print a lista.
-    System.out.printf("ColorList after calling removeColor: %s%n", colorList);
-    
-    //Fim
-}
-//Remove cores especificadas em collection2 a partir de collection1.
-    private static void removeColor(Collection<String> collection1, Collection<String> collection2)
-    {
-        Iterator<String> iterator = collection1.iterator();
-        while(iterator.hasNext())
-        {
-            if(collection2.contains(iterator.next()))
-                iterator.remove();
+        String[] color = {"RED", "BLUE", "WHITE", "MAGENTA"};
+        List<String> list = new ArrayList<String>();
+        for (String string : color) {
+            list.add(string);
         }
+        System.out.printf("List: %s", list);
+        String[] remove = {"BLUE", "WHITE"};
+        List<String> listRemove = new ArrayList<String>();
+        System.out.printf("removeList: %s", listRemove);
     }
 }
