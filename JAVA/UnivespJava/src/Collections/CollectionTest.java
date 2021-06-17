80198c1194db99146409e439526c4be70f23618f
João Alonso <joaoalonso42@gmail.com>
2021-06-17 14:07:09 -0300

    Create CollectionTest.java

---
+++ JAVA/UnivespJava/src/Collections/CollectionTest.java
@@ -0,0 +1,14 @@
+public class CollectionTest {
+    
+    public static void main(String[] args) {
+    // Adiciona elementos no array color a lista (Magenta, Red, White, Blue, Cyan).
+    
+    //Adiciona elementos no array removeColor(Red, White, Blue) em remoList.
+
+    //Print a lista.
+    //Remove da lista as cores contidas em remoList.
+    //Print a lista.
+
+    }
+    
+}
