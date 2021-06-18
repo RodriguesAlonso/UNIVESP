eec43f86a0badad192a5ae30d5df5407dd746074
João Alonso <joaoalonso42@gmail.com>
2021-06-18 11:12:51 -0300

    Create TimeComparator.java

---
+++ JAVA/UnivespJava/src/Collections/TimeComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+pagina 548
+public class TimeComparator implements Comparator<Time2> {
+
+    @Override
+    public int compare(Time2 time1, Time2 time2) {
+        int hourDifference = time1.getHourt() - time2.getHours
+
+
+        return 0;
+    }
+    
+}
