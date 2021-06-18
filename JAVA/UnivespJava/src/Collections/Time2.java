18148f04c39b74d8946c7ecc21c4342fb63eefe5
João Alonso <joaoalonso42@gmail.com>
2021-06-18 13:49:01 -0300

    Update Time2.java

--- JAVA/UnivespJava/src/Collections/Time2.java
+++ JAVA/UnivespJava/src/Collections/Time2.java
@@ -5,12 +5,17 @@ public class Time2 {
 
 
     public Time2() {
+        this(0, 0, 0);
     }
 
     public Time2(int hours, int minute, int second) {
         this.hours = hours;
         this.minute = minute;
         this.second = second;
+    }
+    public Time2(Time2 time)
+    {
+        this(time.getHours(), time.getMinute(), time.getSecond());
     }
 
     public int getHours() {
@@ -52,6 +57,21 @@ public class Time2 {
         return this;
     }
 
+    public void setTime(int hour, int minute, int second)
+    {
+    if (hour < 0 || hour >= 24)
+    throw new IllegalArgumentException("hour must be 0-23");
+
+    if (minute < 0 || minute >= 60)
+    throw new IllegalArgumentException("minute must be 0-59");
+
+    if (second < 0 || second >= 60)
+    throw new IllegalArgumentException("second must be 0-59");
+
+     this.hours = hour;
+     this.minute = minute;
+     this.second = second;
+    }
     @Override
     public boolean equals(Object o) {
         if (o == this)
@@ -61,11 +81,6 @@ public class Time2 {
         }
         Time2 time2 = (Time2) o;
         return hours == time2.hours && minute == time2.minute && second == time2.second;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(hours, minute, second);
     }
 
     @Override
