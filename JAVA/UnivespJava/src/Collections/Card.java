cd27cd4f82252b33df6d20b125b1a09510152fbc
João Alonso <joaoalonso42@gmail.com>
2021-06-18 15:17:28 -0300

    Update Card.java

--- JAVA/UnivespJava/src/Collections/Card.java
+++ JAVA/UnivespJava/src/Collections/Card.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Card {
     public static enum Face {Ace, Deuce, Three, Four, Five, Six,
     Seven, Eight, Nine, Ten, Jack, Queen, King };
@@ -18,5 +22,37 @@ public class Card {
     public String toString()
     {
         return String.format("%s of %s", face, suit);
+    }
+    public class DeckOfCards
+    {
+        private List<Card> list;
+
+        public DeckOfCards()
+        {
+            Card[] deck = new Card[52];
+            int count = 0;
+
+            for (Card.Suit suit : Card.Suit.values())
+            {
+                for (Card.Face face : Card.Face.values())
+                {
+                    deck[count] = new Card(face, suit);
+                    ++count;
+                }
+            }
+            list = Arrays.asList(deck);
+            Collections.shuffle(list);
+        }
+        public void printCards()
+        {
+            for (int i = 0; i < list.size(); i++)
+            {
+                System.out.printf("%-19s%s", list.get(i), ((i + 1) % 4 == 0) ? "%n" : "");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        DeckOfCards cards = new DeckOfCards();
+        cards.printCards();
     }
 }
