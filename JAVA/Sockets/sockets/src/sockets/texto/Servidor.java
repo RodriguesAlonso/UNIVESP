efb0df8e99ef16304407415181f91bdcac2b7ab7
João Alonso <joaoalonso42@gmail.com>
2021-06-20 20:40:03 -0300

    Create Servidor.java

---
+++ JAVA/Sockets/sockets/src/sockets/texto/Servidor.java
@@ -0,0 +1,32 @@
+package sockets.texto;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(54321);
+        System.out.println("A porta 54321 foi aberta!");
+        System.out.println("Servidor esperando receber mensagem de cliente...");
+
+        Socket socket = serverSocket.accept();
+        System.out.println("Cliente " + socket.getInetAddress().getHostAddress() + "conectado");
+
+        DataInputStream entrada = new DataInputStream(socket.getInputStream());
+        String mensagem = entrada.readUTF();
+        String novaMensagem = mensagem.toUpperCase();
+
+        DataOutputStream saida = new DataOutputStream(socket.getOutputStream());
+        saida.writeUTF(novaMensagem);
+
+        entrada.close();
+        saida.close();
+        socket.close();
+
+    }
+}
