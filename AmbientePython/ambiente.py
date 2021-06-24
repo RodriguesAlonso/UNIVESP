66acf9c546cbc26db0eb845cad4ed71338c7d5f4
João Alonso <joaoalonso42@gmail.com>
2021-06-24 16:30:23 -0300

    Update ambiente.py

--- AmbientePython/ambiente.py
+++ AmbientePython/ambiente.py
@@ -7,6 +7,22 @@ def calcPropor(pi, n, f):
 def calcMedia(mediaPopulacao, mediaAmostra, sigma, n):
     calcMed = ((mediaAmostra - mediaPopulacao) / (sigma / sqrt(n)))
     return print(calcMed)
-dp = sqrt(3.4)
-print(dp)
-calcMedia(10, 6.2, 1.84, 10)
+    
+def intersecsão(a, b):
+    res = a*b
+    print (res)
+
+def unicao(a, b):
+    return print(a + b)
+
+def somaDeProbabilidades(a,b):
+    res = a+b - (a * b)
+    print (res)
+
+#unicao(4/36, 5/36)
+#intersecsão(4/6, 5/6)
+#somaDeProbabilidades(4/36, 6/36)
+
+
+#calcMedia(10, 6.2, 1.84, 10)
+#calcPropor
