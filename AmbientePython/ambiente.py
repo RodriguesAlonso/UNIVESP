d5de10dc338834b42734c8d36bb363f575fcc5e9
João Alonso <joaoalonso42@gmail.com>
2021-07-09 15:16:35 -0300

    1

--- AmbientePython/ambiente.py
+++ AmbientePython/ambiente.py
@@ -30,4 +30,21 @@ def probabilidadeCondicional(a, b): #A condiconal B
     res = (a + b) / b
     return res
 
-print(somaDeProbabilidadesDoisEventos(1/4, 1/12))
+lista = [18, 23, 32, 29, 15, 21, 35, 27]
+m = 25
+soma = 0
+soma2 = 0
+n = 8
+for x in lista:
+    print(x - m)    
+    soma = (x - m)**2
+    print(soma)
+    soma2 += soma
+    print(soma2) 
+    #print(soma2)  
+
+print(soma2 / sqrt(n-1))
+print(1.96 * 127 / sqrt(n))
+
+
+
